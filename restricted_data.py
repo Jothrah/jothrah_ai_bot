b1eb904d0ccc7d429f902edfee352693970ffc4b
Jothrah <info@jothrah.com>
2026-03-26 17:27:50 +0300

    Create restricted_data.py

---
+++ restricted_data.py
@@ -0,0 +1,10 @@
+RESTRICTED_PESTICIDES = [
+    {
+        "no": "1",
+        "common_name": "Example Name",
+        "arabic_name": "اسم عربي",
+        "cas_rn": "000-00-0",
+        "status": "مقيد",
+        "main_uses": "F"
+    }
+]
