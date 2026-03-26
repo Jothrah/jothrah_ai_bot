292fe5cc9560ff44b34fbd38c9770ee64da9221e
Jothrah <info@jothrah.com>
2026-03-26 18:17:16 +0300

    Create banned_data.py

---
+++ banned_data.py
@@ -0,0 +1,23 @@
+BANNED_DATABASE = {
+    "ايمزاليل": {
+        "number": "87",
+        "arabic_name": "إيمازاليل",
+        "english_name": "Imazalil",
+        "status": "محظور",
+        "cas": "35554-44-0",
+        "usage": "مبيد فطري",
+        "classification": "مبيد فطري",
+        "main_uses": "F"
+    },
+
+    "ديازينون": {
+        "number": "88",
+        "arabic_name": "ديازينون",
+        "english_name": "Diazinon",
+        "status": "محظور",
+        "cas": "333-41-5",
+        "usage": "مبيد حشري",
+        "classification": "مبيد حشري",
+        "main_uses": "I"
+    }
+}
